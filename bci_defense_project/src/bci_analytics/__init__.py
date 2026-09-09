ddb0554970505dd070594ae34a671a1f9de035c1
salathaniebrown-prog <265334124+salathaniebrown-prog@users.noreply.github.com>
2026-09-09 11:42:53 -0400

    Align BCI analytics package exports with engine API

--- bci_defense_project/src/bci_analytics/__init__.py
+++ bci_defense_project/src/bci_analytics/__init__.py
@@ -1,19 +1,13 @@
-"""Observation-only BCI analytics for Eagle Eyes.
-
-This package analyzes supplied numeric signal samples. It does not connect to
-hardware, stimulate a user, diagnose a condition, or issue commands.
-"""
-
 from .engine import (
-    BCI_ANALYTICS_VERSION,
-    AnalysisResult,
-    analyze_signal,
+    BCIState,
+    analyze_state,
+    binary_signature,
     hamming_distance,
 )
 
 __all__ = [
-    "BCI_ANALYTICS_VERSION",
-    "AnalysisResult",
-    "analyze_signal",
+    "BCIState",
+    "analyze_state",
+    "binary_signature",
     "hamming_distance",
 ]
